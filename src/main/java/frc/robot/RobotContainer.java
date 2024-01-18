a3d8afec30e688879548e053db53db7491bcdf25
Samuel Bowling <ssb00103@ksad-fk42m61wfv.ks.k12itc.us>
2024-01-17 20:30:00 -0600

    Drive code, needs testing (I HATE WPILIB)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,26 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.TankDrive;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
+  private DriveSubsystem driveSubsystem = new DriveSubsystem();
+
+  private Joystick leftJoystick = new Joystick(OperatorConstants.LEFT_JOYSTICK_PORT);
+  private Joystick rightJoystick = new Joystick(OperatorConstants.RIGHT_JOYSTICK_PORT);
+
   public RobotContainer() {
+    driveSubsystem.setDefaultCommand(
+      new TankDrive(
+        () -> -leftJoystick.getY(),
+        () -> -rightJoystick.getY(),
+        driveSubsystem)
+    );
     configureBindings();
   }
 
