509ee600bd719c384c1eff1e38fa6bf2c1006dfc
Zachary Thomas <thomazac000@usd232.org>
2024-02-03 14:42:22 -0600

    started climb

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Constants {
+
+    public class ClinbConstants {
+    
+        
+    }
+    
+}
--- src/main/java/frc/robot/subsystems/climbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climbSubsystem.java
@@ -1,5 +1,28 @@
 package frc.robot.subsystems;
 
-public class climbSubsystem {
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimbSubsystem extends SubsystemBase{
+
+    private Relay climbMotor;
+
+    public ClimbSubsystem() {
+        climbMotor = new Relay(3);
+
+    }
+
+    public void climbUp() {
+        climbMotor.set(Relay.Value.kForward);
+    }
+
+    public void climbDown() {
+        climbMotor.set(Relay.Value.kReverse);
+    }
+
+    public void stop() {
+        climbMotor.set(Relay.Value.kOff);
+    }
+
 
 }
