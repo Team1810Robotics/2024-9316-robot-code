c9c25a8c74d394507ce3f4a919797d3d2a5ec93e
cgathright133 <cgathright@gmail.com>
2024-02-17 13:26:00 -0600

    Added Base Shuffleboard information from test-chassis code.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,13 +7,35 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+
+
 public class RobotContainer {
+  private SendableChooser<Command> autoChooser = new SendableChooser<>();
+
   public RobotContainer() {
+    setShuffleboard();
     configureBindings();
   }
 
-  private void configureBindings() {}
 
+  private void configureBindings() {}
+   
+  public void setShuffleboard() {
+   // Shuffleboard.getTab("Gen").addBoolean("Note Detector", () -> intakeSubsystem.getNoteDetector());
+    
+    autoChooser.setDefaultOption("No Auto", new InstantCommand());
+   // autoChooser.addOption("offline", new Offline(driveSubsystem));
+   // autoChooser.addOption("scoreOffline", new ScoreOffline(driveSubsystem, intakeSubsystem, shooterSubsystem));
+   // autoChooser.addOption("scoreOfflineScore", new ScoreOfflineScore(shooterSubsystem, intakeSubsystem, driveSubsystem));
+  //  autoChooser.addOption("score", new Score(shooterSubsystem, intakeSubsystem));
+    Shuffleboard.getTab("Auto").add("Auto Chooser", autoChooser);
+    
+  }
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
