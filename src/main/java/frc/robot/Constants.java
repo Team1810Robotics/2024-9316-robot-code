e3cff486d782e69ad7efe65e0ad77a9fa25afd40
SBowling4 <bowlisam001@icloud.com>
2024-03-02 17:18:12 +0000

    fixed changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,13 @@ public class Constants {
         public static final int XBOX_CONTROLLER_PORT = 2;
     }
     public class DriveConstants {
-        public static final int BACK_LEFT_MOTOR = 0;
-        public static final int FRONT_LEFT_MOTOR = 1;
+        public static final int BACK_LEFT_MOTOR = 1;
+        public static final int FRONT_LEFT_MOTOR = 2;
         public static final boolean LEFT_INVERTED = true;
 
 
-        public static final int BACK_RIGHT_MOTOR = 2;
-        public static final int FRONT_RIGHT_MOTOR = 3;
+        public static final int BACK_RIGHT_MOTOR = 3;
+        public static final int FRONT_RIGHT_MOTOR = 4;
         public static final boolean RIGHT_INVERTED = false;
 
         public static final int LEFT_SHIFT_SERVO = 0;
