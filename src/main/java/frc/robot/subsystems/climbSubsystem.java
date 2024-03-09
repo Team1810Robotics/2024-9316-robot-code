8cd9c507db19d5188be0cfc2abb2eb67f8c06e40
Zachary Thomas <thomazac000@usd232.org>
2024-03-09 11:00:10 -0600

    added encoder

--- src/main/java/frc/robot/subsystems/climbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climbSubsystem.java
@@ -3,18 +3,21 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimbConstants;
+import edu.wpi.first.wpilibj.Encoder;
 
 
 public class climbSubsystem extends SubsystemBase{
 
     private Victor climbLeftMotor;
     private Victor climbRightMotor;
-    private Victor climbTensionMotor;
+    private Encoder climbEncoder;
+   // private Victor climbTensionMotor;
 
     public climbSubsystem() {
         climbLeftMotor = new Victor(ClimbConstants.CLIMB_LEFT_MOTOR_PORT);
         climbRightMotor = new Victor(ClimbConstants.CLIMB_RIGHT_MOTOR_PORT);
-        climbTensionMotor = new Victor(ClimbConstants.CLIMB_TENSION_MOTOR_PORT);
+        climbEncoder = new Encoder(ClimbConstants.CLIMB_ENCODER_PORT_A , ClimbConstants.CLIMB_ENCODER_PORT_B);
+        //climbTensionMotor = new Victor(ClimbConstants.CLIMB_TENSION_MOTOR_PORT);
     }
 
     public void climbUp() {
